b6332b078681d251c53e3a38e8dba395596872ef
Ubuntu <ubuntu@ip-172-31-21-185.ec2.internal>
2024-06-25 08:15:14 +0000

    env vars added

--- config/admin.js
+++ config/admin.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+          salt: env('API_TOKEN_SALT'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
+
   transfer: {
     token: {
       salt: env('TRANSFER_TOKEN_SALT'),
